6db0f5c588d94e953feea51853ef11f641042ab9
LeNhaAnticodon <quangnha9210@gmail.com>
2025-10-02 23:18:49 +0900

    begin3

---
+++ src/main/java/com/lenha/excel_3bc_toriai/test/WcWidthFull.java
@@ -0,0 +1,8 @@
+package com.lenha.excel_3bc_toriai.test;
+
+public class testChuNhat {
+    public static void main(String[] args) {
+        String japanText = "三洋鋼材(株)";
+        System.out.println(japanText.length());
+    }
+}
---
+++ src/main/java/com/lenha/excel_3bc_toriai/test/WidthCalculator.java
@@ -0,0 +1,2 @@
+package com.lenha.excel_3bc_toriai.test;public class WidthCalculator {
+}
