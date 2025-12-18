76b1525cc58898035098c134d8859d1f42e511da
LeNhaAnticodon <quangnha9210@gmail.com>
2025-12-18 20:14:10 +0900

    begin3

--- src/main/java/com/lenha/excel_3bc_toriai/MainApplication.java
+++ src/main/java/com/lenha/excel_3bc_toriai/MainApplication.java
@@ -14,7 +14,7 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         // Tạo đối tượng Image từ file ảnh (đảm bảo file ảnh nằm trong thư mục resources)
-        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/lenha/excel_3bc_toriai/ICON/LOGO_CHL.png")));
+        Image icon = new Image(Objects.requireNonNull(getClass().getResourceAsStream("/com/lenha/excel_3bc_toriai/ICON/LOGO_excel-3bc.png")));
         // Thiết lập biểu tượng cho Stage
         stage.getIcons().add(icon);
 
